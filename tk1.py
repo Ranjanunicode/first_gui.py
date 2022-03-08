a2c4a8ce154c8f5b758e19251a344369fcc5ec1b
Ranjan Pandit <59310086+Ranjanunicode@users.noreply.github.com>
2022-03-08 12:35:06 +0530

    Update tk1.py

--- tk1.py
+++ tk1.py
@@ -1,10 +1,15 @@
+# importing everything from tkinter module 
 from tkinter import *
 
+# root object of class tk()
 root=Tk()
+# defining the dimensions of the gui window
 root.geometry("655x354")
+# adding title to the window
 root.title("FIRST GUI PROGRAM")
 
 l1=Label(root,text="WELCOME TO THE CODING WORLD")
 l1.pack()
 
+# for display until the window is closed
 root.mainloop()
